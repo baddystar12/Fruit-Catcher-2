d245f18079fcd64fc6834a4cf148174f4a90997d
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-01-23 19:26:54 -0800

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,49 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+    getRankCount(){
+        database.ref("rankCount").on("value", (data)=>{
+            playerRank = data.val();
+        })
+    }
+    updateRankCount(a){
+        database.ref("/").update({
+            rankCount: a
+        })
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
